c4bca9fe56ad306f25cd4016056751f3af63a14a
David Filiks <davidfiliks55@gmail.com>
2025-04-19 19:39:09 +0100

    Working 'tokenization', working on parsing

--- src/simpcsv.c
+++ src/simpcsv.c
@@ -4,115 +4,109 @@
 #include <string.h>
 #include "simpcsv.h"
 
-_SimpCSV_Da* _simpcsv_da_init(size_t capacity)
-{
-    _SimpCSV_Da* da = malloc(sizeof(_SimpCSV_Da));
-    da->m_data = malloc(capacity * sizeof(char*));
-    da->m_capacity = capacity;
-    da->m_size = 0;
-
-    return da;
-}
-
-_SimpCSV_Da* _simpcsv_da_resize(_SimpCSV_Da* target, size_t new_capacity)
-{
-    void* copy = malloc(new_capacity * sizeof(char*));
-    memcpy(target->m_data, copy, sizeof(copy));
-    free(copy);
-    return target;
-}
-
-_SimpCSV_Da* _simpcsv_da_pushback(_SimpCSV_Da* target, char data)
-{
-    if (target->m_capacity == target->m_size)
-    {
-        return NULL; // RESIZE
-    }
-    target->m_data[target->m_size] = data;
-    target->m_size++;
-
-    return target;
-}
-
-_SimpCSV_Da* _simpcsv_da_pop(_SimpCSV_Da* target)
-{
-    assert(target->m_size > 0);
-
-    target->m_size--;
-
-    target = _simpcsv_da_resize(target, target->m_size);
-
-    // RESIZE to take less space
-
-    return target;
-}
-
-void _simpcsv_da_free(_SimpCSV_Da* target)
-{
-    free(target->m_data);
-    free(target);
-}
-
-SimpCSV_Handle* simpcsv_open_file(char* file_name, char delim, char escape)
+SimpCSV_Handle* simpcsv_open_file(char* file_name, char quote, char delim, char escape)
 {
     SimpCSV_Handle* handle = malloc(sizeof(SimpCSV_Handle));
+
+    handle->m_quote = quote;
     handle->m_delim = delim;
     handle->m_escape = escape;
 
     handle->m_file = fopen(file_name, "r");
-    int c;
-    size_t i = 0, size = CONTENT_INCREMENT;
-    handle->m_contents = malloc(size);
 
-    while ((c = fgetc(handle->m_file)) != EOF)
-    {
-        if (i == size - 1)
-        {
-            size += CONTENT_INCREMENT;
-            handle->m_contents = realloc(handle->m_contents, size);
-        }
+    fseek(handle->m_file, 0, SEEK_END);
+    size_t size = ftell(handle->m_file);
+    size += 1;
+    fseek(handle->m_file, 0, SEEK_SET);
 
-        handle->m_contents[i++] = c;
-    }
+    char* buffer = malloc(sizeof(char) * size);
 
-    handle->m_contents[i] = '\0';
+    fread(buffer, sizeof(char), size - 1, handle->m_file);
+
+    buffer[size] = '\0';
+
+    handle->m_contents = strdup(buffer);
 
     return handle;
 }
 
 SimpCSV_Handle* simpcsv_parse_file(SimpCSV_Handle* handle)
 {
-    handle->m_cell = malloc(200 * sizeof(_SimpCSV_Cell));
-//    _SimpCSV_Cell* cell = malloc(sizeof(_SimpCSV_Cell));
-    _SimpCSV_Da* buffer = _simpcsv_da_init(5000);
+    size_t cs = _SIMPCSV_CELL_INCREMENT, bs = _SIMPCSV_CHAR_INCREMENT, ci = 0, bi = 0;
+    size_t delim_c = 1, escape_c = 1;
 
+    handle->m_cell = malloc(cs);
+    char* buffer = malloc(bs);
+
+    // slow
     for (size_t i = 0; i < strlen(handle->m_contents); i++)
     {
-        while (handle->m_contents[i] != handle->m_delim && handle->m_contents[i] != '\0')
+        if (handle->m_contents[i] == handle->m_quote)
         {
-            buffer = _simpcsv_da_pushback(buffer, handle->m_contents[i++]);
+            do
+            {
+                _SIMPCSV_CHAR_CHECK_IF_RESIZE_NEEDED(buffer,
+                                                     bi,
+                                                     bs - 1,
+                                                     _SIMPCSV_CHAR_INCREMENT);
+
+                buffer[bi++] = handle->m_contents[++i];
+            } while (handle->m_contents[i] != handle->m_quote);
+        }
+        while (handle->m_contents[i] != handle->m_delim  &&
+               handle->m_contents[i] != handle->m_escape &&
+               handle->m_contents[i] != '\0')
+        {
+            _SIMPCSV_CHAR_CHECK_IF_RESIZE_NEEDED(buffer,
+                                                 bi,
+                                                 bs - 1,
+                                                 _SIMPCSV_CHAR_INCREMENT);
+
+            buffer[bi++] = handle->m_contents[i++];
         }
 
-        handle->m_cell[i].m_data = buffer->m_data;
-        buffer = _simpcsv_da_resize(buffer, 0);
+        _SIMPCSV_CELL_CHECK_IF_RESIZE_NEEDED(handle->m_cell,
+                                             ci * sizeof(_SimpCSV_Cell),
+                                             cs, _SIMPCSV_CELL_INCREMENT);
+        buffer[bi] = '\0';
+        handle->m_cell[ci].m_col = delim_c;
+        handle->m_cell[ci].m_row = escape_c;
+
+        if (handle->m_contents[i] == handle->m_delim) delim_c++;
+        else if (handle->m_contents[i] == handle->m_escape)
+        {
+            handle->m_number_of_cols = delim_c; // reassigned every single time - slow
+            escape_c++;
+            delim_c = 1;
+        }
+
+        handle->m_number_of_rows = escape_c; // same thing
+
+        handle->m_cell[ci++].m_data = strdup(buffer);
+        bi = 0;
     }
 
-    _simpcsv_da_free(buffer);
+    handle->m_number_of_cells = ci;
+
+    free(buffer);
 
     return handle;
 }
 
-// parse the file, assign a number to each column, assign a column number to each cell. this function will just
-// find all the cells in the specific column and return an array of them.
-// char** simpcsv_get_column(SimpCSV_Handle* handle, const char* column_name);
+size_t simpcsv_get_number_of_columns(SimpCSV_Handle* handle)
+{
+    return handle->m_number_of_cols;
+}
 
-// char** simpcsv_get_row(SimpCSV_Handle* handle, size_t row_number);
-
-// char* simpcsv_get_element(SimpCSV_Handle* handle, const char* column_name, size_t row_number);
+size_t simpcsv_get_number_of_rows(SimpCSV_Handle* handle)
+{
+    return handle->m_number_of_rows;
+}
 
 void simpcsv_close_file(SimpCSV_Handle* handle)
 {
     fclose(handle->m_file);
     free(handle->m_contents);
+    free(handle->m_cell);
     free(handle);
 }
--- src/simpcsv.h
+++ src/simpcsv.h
@@ -3,21 +3,23 @@
 
 #include <stdio.h>
 
-#define CONTENT_INCREMENT (5 * sizeof(char))
+#define _SIMPCSV_CHAR_INCREMENT (5 * sizeof(char))
+#define _SIMPCSV_CELL_INCREMENT (5 * sizeof(_SimpCSV_Cell))
 
-typedef struct {
-    char* m_data;
-    size_t m_capacity;
-    size_t m_size;
-} _SimpCSV_Da;
+#define _SIMPCSV_CELL_CHECK_IF_RESIZE_NEEDED(buffer, index, size, inc)   \
+    if (index == size)                                                   \
+    {                                                                    \
+        size += inc;                                                     \
+        buffer = realloc(buffer, size);                                  \
+    }                                                                    \
 
-_SimpCSV_Da* _simpcsv_da_init(size_t capacity);
-
-_SimpCSV_Da* _simpcsv_da_pushback(_SimpCSV_Da* target, char data);
-
-_SimpCSV_Da* _simpcsv_da_pop(_SimpCSV_Da* target);
-
-void _simpcsv_da_free(_SimpCSV_Da* target);
+#define _SIMPCSV_CHAR_CHECK_IF_RESIZE_NEEDED(buffer, index, size, inc)   \
+    size_t k = size; /* dumb c string stuff */                           \
+    if (index == k)                                                      \
+    {                                                                    \
+        bs += inc;                                                       \
+        buffer = realloc(buffer, bs);                                    \
+    }                                                                    \
 
 typedef struct {
     size_t m_col;
@@ -27,23 +29,34 @@ typedef struct {
 
 typedef struct {
     FILE* m_file;
+    char m_quote;
     char m_delim;
     char m_escape;
     char* m_contents;
     size_t m_cols;
     size_t m_rows;
     _SimpCSV_Cell* m_cell;
+    size_t m_number_of_cells;
+    size_t m_number_of_cols;
+    size_t m_number_of_rows;
 } SimpCSV_Handle;
 
-SimpCSV_Handle* simpcsv_open_file(char* file_name, char delim, char escape);
+SimpCSV_Handle* simpcsv_open_file(char* file_name, char quote, char delim, char escape);
 
 SimpCSV_Handle* simpcsv_parse_file(SimpCSV_Handle* handle);
-//char** simpcsv_get_headers(SimpCSV_Handle* handle);
 
+size_t simpcsv_get_number_of_columns(SimpCSV_Handle* handle);
+
+size_t simpcsv_get_number_of_rows(SimpCSV_Handle* handle);
+
+/* DRAFTS */
+
+//char** simpcsv_get_headers(SimpCSV_Handle* handle);
 // char** simpcsv_get_column(SimpCSV_Handle* handle, const char* column_name);
 // char** simpcsv_get_row(SimpCSV_Handle* handle, size_t row_number);
-
 // char* simpcsv_get_element(SimpCSV_Handle* handle, const char* column_name, size_t row_number);
+
+/* DRAFTS */
 
 void simpcsv_close_file(SimpCSV_Handle* handle);
 
